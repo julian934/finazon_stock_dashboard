a343fc9d2e8b0f827656f01682caed4896b031cd
Julian Thatguy <jborner111@gmail.com>
2024-05-13 18:41:17 -0500

    edits

--- src/app/api/getTimeSeries/route.ts
+++ src/app/api/getTimeSeries/route.ts
@@ -1,4 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "apikey 82d7e7011dd94d09bc862d75e429d9d6nw");
+const requestOptions:any = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+};
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+    try {
+        const res = await fetch('https://api.finazon.io/latest/time_series', requestOptions);
+        const data = await res.json();
+        response.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        response.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+/*import { NextResponse } from "next/server";
 import { type NextApiRequest } from "next";
 const myHeaders = new Headers();
 myHeaders.append("Authorization", "apikey 82d7e7011dd94d09bc862d75e429d9d6nw");
@@ -13,3 +34,4 @@ export async function GET(request:NextApiRequest){
     console.log(response)
     return NextResponse.json(response)
 }
+*/
