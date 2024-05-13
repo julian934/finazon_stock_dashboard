b03c6f844c40b0c2f669e07b151b31fca44ec3dc
Julian Thatguy <jborner111@gmail.com>
2024-05-13 18:00:19 -0500

    sixth commit

--- src/app/api/getFinances/route.ts
+++ src/app/api/getFinances/route.ts
@@ -1,4 +1,5 @@
-export async function fetchDataFromExternalAPI(url:string) {
+import { NextApiRequest,NextApiResponse } from "next";
+/*export async function fetchDataFromExternalAPI(url:string) {
     console.log(url)
     const searchParams=url
     //const query=searchParams.get('ticker')
@@ -16,4 +17,29 @@ export async function fetchDataFromExternalAPI(url:string) {
         throw new Error('Failed to fetch data from external API');
     }
     return await response.json();
+}
+*/
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const apiKey = process.env.NEXT_PUBLIC_YELP_API_KEY;
+        const url = req.query.url as string; // Assuming you're passing the URL as a query parameter
+
+        // Adding headers to the fetch request
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `apikey ${apiKey}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data from external API');
+        }
+
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
