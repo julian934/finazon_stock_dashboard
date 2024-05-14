5fed0380d4128b9d504722460af3a9dfe8c4620c
Julian Thatguy <jborner111@gmail.com>
2024-05-13 21:24:23 -0500

    edit

--- src/app/components/UI/scrollBar/page.tsx
+++ src/app/components/UI/scrollBar/page.tsx
@@ -1,12 +1,15 @@
+// Import necessary modules
 import * as React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 import { cn } from "../../../lib/utils";
 
+// Define props interface
 interface ScrollAreaProps {
   children: React.ReactNode;
   className?: string;
 }
 
+// Define ScrollAreaPage component
 const ScrollAreaPage: React.FC<ScrollAreaProps> = ({ children, className }) => (
   <ScrollAreaPrimitive.Root className={cn("relative overflow-hidden", className)}>
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
@@ -17,18 +20,18 @@ const ScrollAreaPage: React.FC<ScrollAreaProps> = ({ children, className }) => (
   </ScrollAreaPrimitive.Root>
 );
 
-const ScrollBar = React.forwardRef<
+// Define ScrollBar component
+const ScrollBar: React.FC = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
+>(({ orientation = "vertical", ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
     className={cn(
       "flex touch-none select-none transition-colors",
       orientation === "vertical" && "h-full w-2.5 border-l border-l-transparent p-[1px]",
-      orientation === "horizontal" && "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-      className
+      orientation === "horizontal" && "h-2.5 flex-col border-t border-t-transparent p-[1px]"
     )}
     {...props}
   >
@@ -37,4 +40,6 @@ const ScrollBar = React.forwardRef<
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
-export  {ScrollAreaPage}; // Export ScrollAreaPage as default
+// Export components as default
+export default ScrollAreaPage;
+export { ScrollBar };
