7c321b33f9bdc96f62b34ea8ecc8584c1629153e
Julian Thatguy <jborner111@gmail.com>
2024-05-13 23:24:31 -0500

    edit

--- src/app/api/getStockData/route.ts
+++ src/app/api/getStockData/route.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 const myHeaders = new Headers();
 myHeaders.append("Authorization", "apikey 82d7e7011dd94d09bc862d75e429d9d6nw");
+//import Error from "next/error";
+import NextError from "next/error"
+import { NextApiRequest,NextApiResponse } from "next";
 
 const requestOptions:any = {
   method: "GET",
@@ -10,16 +13,17 @@ const requestOptions:any = {
 };
 
 //get Stock Codes, Last Price, Previous Day Price, Price Change, Percent Change, Max Price, Low Price
-export async function GET(){
-   const response:any=fetch("https://api.finazon.io/latest/tickers/us_stocks", requestOptions)
-   .then((response) => response.json())
-   //.then((result) => console.log(result))
-   .catch((error) => console.error(error))
-   //const newData=response //.then((response:any)=>response.json()).then((result:any)=>console.log(result)).catch((error:any)=>console.log(error))
-   //console.log(newData)
-    let newData=JSON.stringify(response.data)
-    let changedData=response.json()
-    console.log(newData)
-    console.log(changedData)
-   return NextResponse.json(changedData)
+export async function getData(req:NextApiRequest,res:NextApiResponse){
+  try {
+    const response = await fetch("https://api.finazon.io/latest/tickers/us_stocks", requestOptions);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+    return res.status(200).json(data);
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({errir:'Failed to fetch data'})
+}
 }
