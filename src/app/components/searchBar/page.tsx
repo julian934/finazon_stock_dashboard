e3186b98a64f2babc05ccad24f2e86964c470ffb
Julian Thatguy <jborner111@gmail.com>
2024-05-13 21:07:00 -0500

    edit

--- src/app/components/searchBar/page.tsx
+++ src/app/components/searchBar/page.tsx
@@ -6,7 +6,8 @@ import { fetchDataFromExternalAPI } from '@/app/api/getMarketList/getData'
 import { getIndividualTicker } from '@/app/utils/getStockData/page'
 import { getStockData } from '@/app/utils/getStockData/page'
 import { StoreStateContext } from '@/app/utils/context/page'
-import  {ScrollAreaPage}  from '../UI/scrollBar/page'
+//import  {ScrollAreaPage}  from '../UI/scrollBar/page'
+import { ScrollAreaPage } from '../UI/scrollBar/page'
 type Props = {}
 
 const SearchBar:React.FC<Props> = (props: any) => {
