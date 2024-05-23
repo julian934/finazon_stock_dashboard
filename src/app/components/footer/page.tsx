e66e147165b5f7b69c329d903604cd6b556e10cd
Julian Thatguy <jborner111@gmail.com>
2024-05-23 13:26:28 -0500

    added links

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -4,17 +4,17 @@ import Image from 'next/image'
 import Github from '../../images/icons8-github-90.png'
 import Instagram from '../../images/icons8-instagram-100.png'
 import LinkedIn from '../../images/icons8-linkedin-100.png'
-import Twitter from '../../images/icons8-twitter-90.png'
+
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
     <div className='flex md:flex-row justify-around md:w-full' >
       
-       <Link className='hover:bg-amber-400 rounded-md' href='/' ><Image className='h-12 w-12  ' src={Github} alt='Github' /></Link>
-       <Link className='hover:bg-amber-400 rounded-md' href='/' ><Image className='h-12 w-12' src={Instagram} alt='Instagram' /></Link>
-       <Link className='hover:bg-amber-400 rounded-md' href='/' ><Image className='h-12 w-12' src={LinkedIn} alt='LinkedIn' /></Link>
-       <Link className='hover:bg-amber-400 rounded-md' href='/' ><Image className='h-12 w-12' src={Twitter} alt='Twitter' /></Link>
+       <Link className='hover:bg-amber-400 rounded-md' href='https://github.com/julian934/' ><Image className='h-12 w-12  ' src={Github} alt='Github' /></Link>
+       <Link className='hover:bg-amber-400 rounded-md' href='https://www.instagram.com/jbthedev/' ><Image className='h-12 w-12' src={Instagram} alt='Instagram' /></Link>
+       <Link className='hover:bg-amber-400 rounded-md' href='https://www.linkedin.com/in/julian-borner-709b91b7/' ><Image className='h-12 w-12' src={LinkedIn} alt='LinkedIn' /></Link>
+       
       </div>
   )
 }
